fb5dedb2ea72084022daf8fd05789a30ca7490b2
hhusein-alt <mvcskqkago@rambler.ru>
2025-11-01 03:20:54 +0400

    Add Vercel Analytics integration

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Analytics } from '@vercel/analytics/react';
 import Home from './pages/Home';
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Home />
+      <Analytics />
+    </>
+  );
 }
 
 export default App;
